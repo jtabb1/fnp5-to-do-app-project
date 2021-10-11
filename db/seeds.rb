7066bda1ec0986b353faa630a0c2307a2e041657
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-10-11 03:42:38 -0400

    Add more fields to serializers, destroy only starter typed todos upon seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-puts "🔥 Destroying starter todos..."
+puts "🔥 Destroying starter typed todos..."
 StarterTypedTodo.destroy_all
 
-puts "🔥 Destroying todos..."
-Todo.destroy_all
+# puts "🔥 Destroying todos..."
+# Todo.destroy_all
 
-puts "🔥 Destroying types..."
-Type.destroy_all
+# puts "🔥 Destroying types..."
+# Type.destroy_all
 
-puts "🔥 Destroying users..."
-User.destroy_all
+# puts "🔥 Destroying users..."
+# User.destroy_all
 
 puts "🌱 Seeding starter typed todos..."
-StarterTypedTodo.create!(type_name: "coding", to_display: "complete feature a on project (coding)", todo_name: "complete feature a on project")
-StarterTypedTodo.create!(type_name: "coding", to_display: "complete feature bb on project (coding)", todo_name: "complete feature bb on project")
-StarterTypedTodo.create!(type_name: "coding", to_display: "complete feature ccc on project (coding)", todo_name: "complete feature ccc on project")
-StarterTypedTodo.create!(type_name: "coding", to_display: "complete feature dddd on project (coding)", todo_name: "complete feature dddd on project")
+StarterTypedTodo.create!(type_name: "development", to_display: "complete feature a on project (development)", todo_name: "complete feature a on project")
+StarterTypedTodo.create!(type_name: "development", to_display: "complete feature bb on project (development)", todo_name: "complete feature bb on project")
+StarterTypedTodo.create!(type_name: "development", to_display: "complete feature ccc on project (development)", todo_name: "complete feature ccc on project")
+StarterTypedTodo.create!(type_name: "development", to_display: "complete feature dddd on project (development)", todo_name: "complete feature dddd on project")
 StarterTypedTodo.create!(type_name: "eating", to_display: "go to the store (eating)", todo_name: "go to the store")
 StarterTypedTodo.create!(type_name: "eating", to_display: "wash dishes (eating)", todo_name: "wash dishes")
 StarterTypedTodo.create!(type_name: "eating", to_display: "take out the trash (eating)", todo_name: "take out the trash")
